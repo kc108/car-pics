1bf7e40d11a88e1b6325eb127782c6c35ce39ed2
Kim Carpico <kcarpico@gmail.com>
2021-10-18 09:16:54 -0700

    created onInputChange to handle onChange function for form

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,18 +1,38 @@
 import React from "react";
 
 class SearchBar extends React.Component {
+  // *** LONGER APPROACH SEE BELOW ***
+  onInputChange(event) {
+    console.log(event.target.value);
+  }
+
   render() {
     return (
       <div className=" ui segment">
         <form className="ui form">
           <div className="field">
             <label>Image Search</label>
-            <input type="text" />
+            <input type="text" onChange={this.onInputChange} />
           </div>
         </form>
       </div>
     );
   }
 }
+
+// Abbreviated syntax. Alternate way to do this.
+//   render() {
+//     return (
+//       <div className=" ui segment">
+//         <form className="ui form">
+//           <div className="field">
+//             <label>Image Search</label>
+//             <input type="text" onChange={(e) => console.log(e.target.value)} />
+//           </div>
+//         </form>
+//       </div>
+//     );
+//   }
+// }
 
 export default SearchBar;
